280df1eb42534bad5efa8601f526ae499c672c31
Evan Pearson <parsnbl@icloud.com>
2023-11-06 10:34:40 -0800

    handled conflicts

--- src/client/store.js
+++ src/client/store.js
@@ -1,27 +1,17 @@
 //simple store for testing
 
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './slices/userSlice.js';
 import { authApi } from './slices/authService';
+import postSlice from './slices/postSlice';
+import userSlice from './slices/userSlice';
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
+    post: postSlice,
+    user: userSlice,
     [authApi.reducerPath]: authApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(authApi.middleware),
 });
 export default store;
-import { configureStore } from '@reduxjs/toolkit';
-
-//import the slices
-import postSlice from './postSlice';
-import userSlice from './userSlice';
-
-export const store = configureStore({
-  reducer: {
-    post: postSlice,
-    user: userSlice,
-  },
-});
