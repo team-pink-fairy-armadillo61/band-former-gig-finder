57fea037b3604e14c836e06c1b8229594ff2f091
Harold Reeves <haroldreeves@gmail.com>
2023-11-06 10:26:52 -0800

    commit harold

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App.jsx';
+import store from './store.js';
+import { Provider } from 'react-redux';
 
 render(
   <Provider store={store}>
